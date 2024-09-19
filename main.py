89ca95b162114963550279523748b04b02b0614f
gokulcivil <gokulcivil@yahoo.com>
2024-09-19 09:44:17 +0300

    Current weekno

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 
+import imp
+from re import M
 from myPackage import mymodule
 
 from myPackage import add
+import pathlib as Path
 
 mymodule.myfunc()
+file_path = r"D:\SAB\01.RSMLI\17.Daily Progress Report\425\Manpower Category.csv"
+df = add.manpower_category(man_catg_filepath=file_path)
+print(df.head())
 
-
-li = [1,3,5,2]
-li2 = [ 45,65,85]
-print(add.add(*li))
-print(add.add(*li2))
 
--- myPackage/add.py
+++ myPackage/add.py
@@ -5,3 +5,19 @@ def add(*args):
     for i in args:
         result = result + i
     return result
+
+
+import pandas as pd
+import pathlib as Path
+
+def manpower_category( man_catg_filepath):
+    data = []
+    with open(man_catg_filepath, 'r') as f:
+        header = f.readline().strip().split(',')
+        print(header)
+        for line in f:
+            val = line.strip().split(',')
+            data.append(dict(zip(header,val)))
+    df = pd.DataFrame(data)
+    df.set_index('Index',inplace=True)
+    return df
